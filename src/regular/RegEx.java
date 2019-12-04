5084ce14121dc8477a07379eb039b2899ca51461
harshit-arya612 <harshit.arya_cs18@gla.ac.in>
2019-12-04 14:19:33 +0530

    pattern matching done

---
+++ src/regular/RegEx.java
@@ -0,0 +1,9 @@
+package regular;
+import java.util.regex.*;
+public class RegEx {
+    public static void main(String args[])
+    {
+        //System.out.println(Pattern.matches("[a-zA-Z0-9]{7}","arun32"));
+        System.out.println(Pattern.matches("[0-9]{10}","987456321"));
+    }
+}
